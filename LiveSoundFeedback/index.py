a20e1dad1885aef0acd9e60f7a10b0105c06fa7a
Rex fender Baird <fenderrex@gmail.com>
2016-06-22 23:39:56 -0700

    x

--- LiveSoundFeedback/index.py
+++ LiveSoundFeedback/index.py
@@ -7,5 +7,14 @@ app = Flask(__name__, static_url_path='')
 def send_js(path):
     return send_from_directory('static', path)
 
+app.route('/static/js/<path:path>')
+def send_js(path):
+    return send_from_directory('static/js', path)
+
+app.route('/static/js/lib/<path:path>')
+def send_js(path):
+    return send_from_directory('static/js/lib', path)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0",port=4000)
+
